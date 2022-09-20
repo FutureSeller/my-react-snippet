10cabf673eb017b3ee5d2808cc2f096d7935d18f
FutureSeller <f.s3ll3r@gmail.com>
2022-09-20 18:57:25 +0900

    add use-timeout

--- packages/my-custom-hooks/src/index.ts
+++ packages/my-custom-hooks/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./use-previous";
 export * from "./use-callback-ref";
+export * from "./use-timeout";
---
+++ packages/my-custom-hooks/src/use-timeout/index.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useCallbackRef } from "../use-callback-ref";
+
+export function useTimeout(
+  callback: (...args: any[]) => void,
+  delay: number | null
+) {
+  const fn = useCallbackRef(callback);
+
+  useEffect(() => {
+    if (delay == null) return;
+
+    let timeoutId: number | null = null;
+    timeoutId = window.setTimeout(() => {
+      fn();
+    }, delay);
+
+    return () => {
+      if (timeoutId) {
+        window.clearTimeout(timeoutId);
+      }
+    };
+  }, [delay, fn]);
+}
