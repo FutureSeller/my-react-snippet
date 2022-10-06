3717ec762f8add5c24ea37b5d759a90ddfa2b07f
FutureSeller <f.s3ll3r@gmail.com>
2022-10-06 21:34:07 +0900

    add storybook

---
+++ apps/docs/.storybook/main.js
@@ -0,0 +1,36 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      esbuild: {
+        ...config.esbuild,
+        jsxInject: `import React from 'react'`,
+      },
+      rollupOptions: {
+        ...config.rollupOptions,
+        // Externalize deps that shouldn't be bundled
+        external: ["react", "react-dom"],
+        output: {
+          // Global vars to use in UMD build for externalized deps
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+      },
+    };
+  },
+};
